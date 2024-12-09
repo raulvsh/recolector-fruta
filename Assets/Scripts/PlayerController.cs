c2d6b9890ff5247ff06064c27b1929ad361fa277
raulvsh <raulvsh@gmail.com>
2024-12-10 00:34:23 +0100

    feat: apple and bowl assets imported feat: floor and walls added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidadX = 2.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Juego de recolección de frutas");
+        velocidadX = 5.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Translate(-velocidadX * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Translate(velocidadX * Time.deltaTime, 0, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Colision");
+    }
+
+}
