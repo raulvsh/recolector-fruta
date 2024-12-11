fbb651aa67bb3d6f1b3c36d66755ee9118449104
raulvsh <raulvsh@gmail.com>
2024-12-11 12:30:10 +0100

    feat: Actualizada NewScene

---
+++ Assets/script1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class script1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameObject.transform.Translate(-3,-3,-3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
